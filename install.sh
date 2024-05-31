efc4fd53a4c6563032bed0db658da721e4bf0966
goossens32 <cgp00@iesemilidarder.com>
2024-05-31 07:21:13 +0000

    Added get_utils

--- install.sh
+++ install.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-# WEBADMIN INSTALLER
+# WEBADMIN INSTALLER, reccomended UPDATE and UPGRADE before execution of script!
 
-# 
+# Get services
 get_utils() {
-    
+
+    if [ ! -d /etc/apache2 ]; then
+        echo "Apache no está instalado ..."
+        sudo apt install apache2
+    else
+        echo "Apache está instalado"
+    fi;
+
+    if [ ! -d /etc/mysql ]; then
+        echo "MySQL no está instalado ..."
+        sudo apt install mysql-server
+    else
+        echo "MySQL está instalado"
+    fi;
+
+    if [ ! -d /etc/php ]; then
+        echo "PHP no está instalado"
+        sudo apt install php
+    else
+        echo "PHP está instalado"
+    fi;
+
 }
+
+init() {
+    get_utils
+}
+
+init
